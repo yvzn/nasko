1bc9da731ca1239dc1040f96582dc6364281fc2c
yvzn <mail@ludeo.net>
2023-04-19 07:46:41 +0200

    feat: favicon nightmare :)

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,27 @@
-const { minify } = require("terser");
-const sass = require("sass");
+const { minify } = require("terser")
+const sass = require("sass")
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addNunjucksAsyncFilter("jsmin", jsmin);
-	eleventyConfig.addPassthroughCopy("src/assets");
-	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addNunjucksAsyncFilter("jsmin", jsmin)
+	eleventyConfig.addPassthroughCopy("src/assets")
+	eleventyConfig.addTemplateFormats("scss")
 
 	eleventyConfig.addExtension("scss", {
 		outputFileExtension: "css",
 
 		compile: async function (inputContent) {
-			const result = sass.compileString(inputContent, { sourceMap: false });
+			const result = sass.compileString(inputContent, { sourceMap: false })
 			return async () => {
-				return result.css;
-			};
+				return result.css
+			}
 		}
-	});
+	})
 
 	eleventyConfig.addCollection("articlesFrancaisTries", function (collectionApi) {
 		return collectionApi.getFilteredByTag("article_francais").sort(function (a, b) {
 			return b.data.daysLeft - a.data.daysLeft
 		})
-	});
+	})
 
 	// Return your Object options:
 	return {
@@ -29,15 +29,15 @@ module.exports = function (eleventyConfig) {
 			input: "src",
 		}
 	}
-};
+}
 
 async function jsmin(code, callback) {
 	try {
-		const minified = await minify(code);
-		callback(null, minified.code);
+		const minified = await minify(code)
+		callback(null, minified.code)
 	} catch (error) {
-		console.error("Terser error: ", error);
+		console.error("Terser error: ", error)
 		// Fail gracefully.
-		callback(null, code);
+		callback(null, code)
 	}
 }
