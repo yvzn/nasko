97cbfd94031f1cb6a4a654d3d8e3161bc168277e
yvzn <mail@ludeo.net>
2023-06-19 16:33:01 +0200

    feat: add fetch support, if available

---
+++ src/js/http.js
@@ -0,0 +1,56 @@
+function xhrGet(url, responseType, successCallback, errorCallback) {
+	var request = new XMLHttpRequest()
+	request.addEventListener("load", requestListener)
+	request.addEventListener("error", errorCallback)
+	request.addEventListener("timeout", errorCallback)
+	request.timeout = 5 * 1000
+	request.responseType = responseType
+	request.open("GET", url, true)
+	request.send()
+
+	function requestListener() {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				successCallback(request.response)
+			} else {
+				errorCallback(request.statusText)
+			}
+		}
+	}
+}
+
+function isFetchSupported() {
+	try {
+		return Boolean(window.fetch)
+	} catch (e) {
+		return false
+	}
+}
+
+function fetchGet(url, responseType, successCallback, errorCallback) {
+	var controller = new AbortController()
+
+	var timeout = setTimeout(function () { controller.abort() }, 5 * 1000)
+
+	fetch(url, { signal: controller.signal })
+		.then(function (response) {
+			if (!response.ok) {
+				errorCallback(response)
+			} else if (responseType === 'json') {
+				response.json().then(successCallback).catch(errorCallback)
+			} else if (responseType === 'text') {
+				response.text().then(successCallback).catch(errorCallback)
+			}
+		})
+		.catch(errorCallback)
+		.finally(function () { clearTimeout(timeout) });
+}
+
+// eslint-disable-next-line no-unused-vars
+var http = {
+	get: xhrGet
+}
+
+if (isFetchSupported()) {
+	http.get = fetchGet
+}
