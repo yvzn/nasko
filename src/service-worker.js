939af32c3632e36f5d980d4fdd1254a0b46102e3
yvzn <mail@ludeo.net>
2023-06-22 09:45:29 +0200

    Revert "feat: network-first service worker"
    This reverts commit 1d2f1f2db8971ddfccb745b0035af87ace4abf58.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,106 +1,80 @@
-// https://github.com/mdn/serviceworker-cookbook/tree/master/strategy-network-or-cache
-
-// eslint-disable-next-line no-undef
-var P = Promise
-
-var CACHE = 'v1'
-
-// On install, cache some resource.
-self.addEventListener('install', function (evt) {
-	// Ask the service worker to keep installing until the returning promise
-	// resolves.
-	evt.waitUntil(precache())
-})
-
-// Enable navigation preload
-function enableNavigationPreload() {
-	if (self.registration.navigationPreload) {
-		return self.registration.navigationPreload.enable();
-	}
-	return P.resolve();
+const addResourcesToCache = async (resources) => {
+	const cache = await caches.open("v1")
+	await cache.addAll(resources)
 }
 
-self.addEventListener("activate", function (evt) {
-	evt.waitUntil(enableNavigationPreload())
+const putInCache = async (request, response) => {
+	const cache = await caches.open("v1")
+	await cache.put(request, response)
+}
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+	// First try to get the resource from the cache
+	const responseFromCache = await caches.match(request)
+	if (responseFromCache) {
+		return responseFromCache
+	}
+
+	// Next try to use (and cache) the preloaded response, if it's there
+	const preloadResponse = await preloadResponsePromise
+	if (preloadResponse) {
+		console.info("using preload response", preloadResponse)
+		putInCache(request, preloadResponse.clone())
+		return preloadResponse
+	}
+
+	// Next try to get the resource from the network
+	try {
+		const responseFromNetwork = await fetch(request)
+		// response may be used only once
+		// we need to save clone to put one copy in cache
+		// and serve second one
+		putInCache(request, responseFromNetwork.clone())
+		return responseFromNetwork
+	} catch (error) {
+		const fallbackResponse = await caches.match(fallbackUrl)
+		if (fallbackResponse) {
+			return fallbackResponse
+		}
+		// when even the fallback response is not available,
+		// there is nothing we can do, but we must always
+		// return a Response object
+		return new Response("Network error happened", {
+			status: 408,
+			headers: { "Content-Type": "text/plain" },
+		})
+	}
+}
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
+	if (self.registration.navigationPreload) {
+		await self.registration.navigationPreload.enable()
+	}
+}
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(enableNavigationPreload())
 })
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function (evt) {
-	// Try network and if it fails, go for the cached copy.
-	evt.respondWith(
-		fromNetwork(evt.request, 400)
-			.catch(function () {
-				return fromPreloadResponse(evt.request, evt.preloadResponse)
-					.catch(function () {
-						return fromCache(evt.request)
-							.catch(function () {
-								return fromCache('/408/')
-							})
-					})
-			}))
-})
-
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-function precache() {
-	return caches.open(CACHE).then(function (cache) {
-		return cache.addAll([
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		addResourcesToCache([
 			"/",
 			"/408/",
 			"/css/style.css",
 			"/assets/hi-res/crib.png",
-			"/assets/hi-res/corgi.png",
-			"/assets/favicon.ico",
-			"/assets/favicon.svg"
+			"/assets/hi-res/corgi.png"
 		])
-	})
-}
+	)
+})
 
-function putInCache(request, response) {
-	return caches.open(CACHE).then(function (cache) {
-		return cache.put(request, response)
-	})
-}
-
-// Time limited network request. If the network fails or the response is not
-// served before timeout, the promise is rejected.
-function fromNetwork(request, timeout) {
-	return new P(function (fulfill, reject) {
-		// Reject in case of timeout.
-		var timeoutId = setTimeout(reject, timeout)
-		// Fulfill in case of success.
-		fetch(request).then(function (response) {
-			clearTimeout(timeoutId)
-			putInCache(request, response.clone())
-			fulfill(response)
-			// Reject also if network fetch rejects.
-		}, reject)
-	})
-}
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-	return caches.open(CACHE).then(function (cache) {
-		return cache.match(request).then(function (matching) {
-			return matching || P.reject('no-match')
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		cacheFirst({
+			request: event.request,
+			preloadResponsePromise: event.preloadResponse,
+			fallbackUrl: "/408/"
 		})
-	})
-}
-
-function fromPreloadResponse(request, preloadResponsePromise) {
-	return new P(function (fulfill, reject) {
-		if (preloadResponsePromise) {
-			preloadResponsePromise.then(function (response) {
-				if (response) {
-					putInCache(request, response.clone())
-					fulfill(response)
-				}
-				reject('no-preload-response')
-			}, reject)
-		}
-		reject('no-preload-response')
-	})
-}
+	)
+})
